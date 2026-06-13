ffe8198a5974df5c9954a40c8f01c232afe3b428
Janus BR <janus.bre@gmail.com>
2026-06-13 19:42:01 +0200

    Initial commit

---
+++ SpriteMove.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class SpriteMove : Sprite2D
+{
+	private const float Speed = 100.0f;
+	public override void _Process(double delta)
+	{
+		Vector2 direction = Vector2.Zero;
+
+		if (Input.IsActionPressed("ui_right"))
+			direction.X += 1;
+		if (Input.IsActionPressed("ui_left"))
+			direction.X -= 1;
+		if (Input.IsActionPressed("ui_down"))
+			direction.Y += 1;
+		if (Input.IsActionPressed("ui_up"))
+			direction.Y -= 1;
+
+		if (direction != Vector2.Zero)
+		{
+			direction = direction.Normalized();
+			Position += direction * Speed * (float)delta;
+		}
+	}
+}
